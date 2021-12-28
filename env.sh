bdc49c920e03a45d08b2c6d0f66feebb60a520ba
Kasper Munch <kaspermunch@birc.au.dk>
2021-12-28 18:35:43 +0100

    Updated env spec

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
 
-conda create -n maps -c conda-forge numpy pandas matplotlib seaborn cartopy geoplot geopandas adjusttext jupyter jupyterlab openpyxl
+conda create -n maps -c conda-forge numpy pandas matplotlib seaborn cartopy geoplot geopandas adjusttext jupyter jupyterlab openpyxl networkx
